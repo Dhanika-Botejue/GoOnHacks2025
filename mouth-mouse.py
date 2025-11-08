4018aafca6af25c73a98be48ac177f6561ef9b30
Dhanika Botejue <dhanikabotejue@gmail.com>
2025-11-08 16:13:47 -0500

    feat: gree dot on mouth center

--- mouth-mouse.py
+++ mouth-mouse.py
@@ -23,9 +23,22 @@ while True:
         for face_landmarks in results.multi_face_landmarks:
             h, w, _ = frame.shape
 
-            # Get upper and lower lip coordinates
-            upper_lip = face_landmarks.landmark[13]  # Approx upper lip center
-            lower_lip = face_landmarks.landmark[14]  # Approx lower lip center
+            # Get mouth landmarks
+            upper_lip = face_landmarks.landmark[13]  # Upper lip center
+            lower_lip = face_landmarks.landmark[14]  # Lower lip center
+            right_corner = face_landmarks.landmark[61]  # Right mouth corner
+            left_corner = face_landmarks.landmark[291]  # Left mouth corner
+
+            # Calculate mouth center (average of key mouth points)
+            mouth_center_x = (upper_lip.x + lower_lip.x + right_corner.x + left_corner.x) / 4
+            mouth_center_y = (upper_lip.y + lower_lip.y + right_corner.y + left_corner.y) / 4
+
+            # Convert to pixel coordinates
+            mouth_center_px = (int(mouth_center_x * w), int(mouth_center_y * h))
+
+            # Draw green dot at mouth center
+            cv2.circle(frame, mouth_center_px, 8, (0, 255, 0), -1)  # Green filled circle
+            cv2.circle(frame, mouth_center_px, 10, (0, 255, 0), 2)  # Green outline circle
 
             upper_y = int(upper_lip.y * h)
             lower_y = int(lower_lip.y * h)
